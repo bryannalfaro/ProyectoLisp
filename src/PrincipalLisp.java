38ced2f8a4d88eab95e67f3c64ff05bd30205ca5
Bryann Alfaro <alf19372@uvg.edu.gt>
2020-03-12 13:56:15 -0600

    main

--- src/PrincipalLisp.java
+++ src/PrincipalLisp.java
@@ -1,4 +1,26 @@
+import java.util.Scanner;
 
 public class PrincipalLisp {
+	
+	public static void main(String[] args) {
+		System.out.println("Bienvenido al interprete Lisp");
+		System.out.println("1. Buscar archivo");
+		System.out.println("2. Salir");
+		Scanner ingreso = new Scanner(System.in);
+		
+		int entrada;
+		entrada=ingreso.nextInt();
+		boolean flag=true;
+		while(flag) {
+			if(entrada==1) {
+				System.out.println("Ingrese path del programa Lisp a ejecutar: ");
+				flag=false;
+			}else if(entrada==2) {
+				System.out.println("Hasta pronto");
+			}else {
+				System.out.println("Opcion no valida");
+			}
+		}
+	}
 
 }
