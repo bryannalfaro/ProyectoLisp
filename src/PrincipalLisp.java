deb2ffe24db9b9846a341c3e4d408fcf7a410a2e
Bryann Alfaro <alf19372@uvg.edu.gt>
2020-03-15 09:25:29 -0600

    lectura del archivo txt con instruccion aritmetica

--- src/PrincipalLisp.java
+++ src/PrincipalLisp.java
@@ -1,3 +1,4 @@
+import java.io.File;
 import java.util.Scanner;
 
 public class PrincipalLisp {
@@ -14,9 +15,28 @@ public class PrincipalLisp {
 		while(flag) {
 			if(entrada==1) {
 				System.out.println("Ingrese path del programa Lisp a ejecutar: ");
+				Scanner ingresoPath=new Scanner(System.in);
+				String ruta=ingresoPath.nextLine();
+				String datos="";
+				if(new File(ruta).exists()) {
+					try {
+				        Scanner inputScan = new Scanner(new File(ruta));
+				        
+				        while (inputScan.hasNextLine()) {
+				            String line = inputScan.nextLine();
+				            datos+=line+"\n";
+				              
+				        }
+				        inputScan.close();
+				    } catch (Exception ex) {
+				        ex.printStackTrace();
+				    }
+				}
+				System.out.println(datos);
 				flag=false;
 			}else if(entrada==2) {
 				System.out.println("Hasta pronto");
+				flag=false;
 			}else {
 				System.out.println("Opcion no valida");
 			}
