cd69059406e5477cf3cd31cf5104bae1fe1ef1e5
Bryann Alfaro <alf19372@uvg.edu.gt>
2020-03-19 16:45:58 -0600

    procesos aparte

--- src/PrincipalLisp.java
+++ src/PrincipalLisp.java
@@ -10,6 +10,7 @@ public class PrincipalLisp {
 		Scanner ingreso = new Scanner(System.in);
 		int entrada;
 		boolean flag=true;
+		Proceso proceso= new Proceso();
 		
 		System.out.println("Bienvenido al interprete Lisp");
 		System.out.println("1. Buscar archivo");
@@ -77,73 +78,8 @@ public class PrincipalLisp {
 			                tempIns.add(archivo.getTarea(archivo.getToken(" \n", instruccions.get(control).toString() )));
 			                
 			            }
-			            for (int i=0;i<tempIns.size();i++) {
-
-			                List instruccion = null;
-			                
-			                if(tempIns.get(i).getClass()==(ArrayList.class)){
-			                    instruccion = (List) tempIns.get(i);
-			                }
-			                else if(tempIns.get(i).getClass()==String.class){
-			                    instruccion = Arrays.asList(tempIns.toString().split(" "));
-			                   
-			                }
-			                else{
-			                     instruccion = tempIns;
-			                }
-			                System.out.println("Instruccion"+instruccion);
-			                
-			                if (instruccion.contains("+") || instruccion.contains("-") || instruccion.contains("*") || instruccion.contains("/")){
-			                	
-			                    OperacionesAritmeticas calculator = new OperacionesAritmeticas();
-			                    System.out.println("Resultado: " + calculator.calcular(instruccion));
-			                    break;
-			                    
-			                } else if (instruccion.contains("list")){
-			                	
-			                    list = new Evaluaciones().list(instruccion.subList(1, instruccion.size()));
-			                    System.out.println("Lista Creada: " + list.get(0));
-			                    
-			                } else if (instruccion.contains("equal")){
-			                	
-			                    if( (new Evaluaciones()).equals(instruccion.get(1), instruccion.get(2))){
-			                        System.out.print("Resultado: Verdadero, " + instruccion.get(1) + " es igual que" + instruccion.get(2));
-			                    }
-			                    else{
-			                        System.out.print("Resultado: Falso, " + instruccion.get(1) + " No es igual que" + instruccion.get(2));
-			                    }
-			                    
-			                } else if (instruccion.contains(">")){
-			                	
-			                    if( (new Evaluaciones()).greaterThan(instruccion.get(1), instruccion.get(2))){
-			                        System.out.print("Resultado: Verdadero, " + instruccion.get(1) + " es mayor que" + instruccion.get(2));
-			                    }
-			                    else{
-			                        System.out.print("Resultado: Falso, " + instruccion.get(1) + " NO es mayor que" + instruccion.get(2));
-			                    }
-			                    
-			                } else if (instruccion.contains("<")){
-			                	
-			                    if( (new Evaluaciones()).lessThan(instruccion.get(1), instruccion.get(2))){
-			                        System.out.print("Resultado: Verdadero, " + instruccion.get(1) + " es menor que" + instruccion.get(2));
-			                    }
-			                    else{
-			                        System.out.print("Resultado: Falso, " + instruccion.get(1) + " NO es menor que" + instruccion.get(2));
-			                    }
-			                
-				            }else if (instruccion.contains("cond")){
-				                	new Evaluaciones().cond(instruccion);
-				            }else if(instruccion.contains("quote")) {
-				            	new Evaluaciones().quoteShow(instruccion);
-				            }else if(instruccion.contains("setq")) {
-				            	new Evaluaciones().setqEstablished(instruccion);
-				            }
-				            else if(instruccion.contains("first")) {
-				            	list=new Evaluaciones().list(instruccion.subList(1, instruccion.size()).subList(0, instruccion.size()-1));
-				            	new Evaluaciones().firstOfList(instruccion,list.get(0).toString().substring(7,8));
-				            }
-			                
-			                }}catch(Exception e){
+			            proceso.procesar(instruccions, tempIns);
+			            }catch(Exception e){
 			                	System.out.println("No");
 			                }flag=false;
 			}else if(entrada==2) {
