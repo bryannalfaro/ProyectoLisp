b9e8dbefcd18b1fbf92a962cac31af92b1c3000b
Estuardo07 <53918904+Estuardo07@users.noreply.github.com>
2020-03-20 23:02:58 -0600

    Add files via upload

--- src/Test.java
+++ src/Test.java
@@ -6,7 +6,45 @@ import java.util.List;
 public class Test {
 	
 	
+	List<String> dato = new ArrayList();
+
+	@Test
+	public void testMayor() {
+		Evaluaciones test = new Evaluaciones();
+		boolean result = test.Mayor(3, 1);
+		assertEquals(true, result);
+	}
 	
+	@Test
+	public void testMenor() {
+		Evaluaciones test = new Evaluaciones();
+		boolean result = test.Menor(3, 1);
+		assertEquals(false, result);
+	}
+	
+	@Test
+	public void testEquals() {
+		Evaluaciones test = new Evaluaciones();
+		boolean result = test.equals(4, 4);
+		assertEquals(true, result);
+	}
+	
+	@Test
+	public void testQuote() {
+		Evaluaciones test = new Evaluaciones();
+		dato.add("e");
+		dato.add("3fd");
+		test.quoteShow(dato);
+	}
+	
+	@Test
+	public void testSetq() {
+		Evaluaciones test = new Evaluaciones();
+		dato.add("setq");
+		dato.add("x");
+		dato.add("4");
+		test.setqEstablished(dato);
+	}
 	
 	
 }
